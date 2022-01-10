30c7bc76942d01e76e91a459cb1e06a6c7674976
flaviogabrielsc <flaviogabriel112890@gmail.com>
2022-01-10 15:49:43 -0300

    config-mongoose-and-post

--- index.js
+++ index.js
@@ -1,64 +1,95 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const character = require('./models/Character')
 const app = express();
+
+try {
+  mongoose.connect(
+    "mongodb+srv://root:123@cluster0.cyrc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  console.log('Banco de dados conectado!')
+
+} catch (err){
+  console.log(`Erro ao conectar ao bando de dados ${err}`)
+}
+
+
+
+
 
 app.use(express.json());
 
 const characters = [
   {
     id: 1,
-    name: 'Harry Potter',
-    spacies: 'Human',
-    house: 'Gryffindor',
-    actor: 'Daniel Redcliffe',
+    name: "Harry Potter",
+    species: "Human",
+    house: "Gryffindor",
+    actor: "Daniel Redcliffe",
   },
   {
     id: 2,
-    name: 'Hermione ',
-    spacies: 'Human',
-    house: 'Gryffindor',
-    actor: 'Emma Watson ',
+    name: "Hermione ",
+    species: "Human",
+    house: "Gryffindor",
+    actor: "Emma Watson ",
   },
 ];
 
 //GET - READ
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(characters.filter(Boolean));
 });
 
 //POST - CREATE
-app.post('/create', (req, res) => {
-  const character = req.body;
+app.post("/create", async (req, res) => {
+  const {name, species, house, actor} = req.body;
 
-  character.id = characters.length + 1;
-  characters.push(character);
+  if(!name || !species || !house || !actor){
+    res.status(400).send({ message: 'Você não enviou todos os dados necessários para o cadastro'})
+    return;
+  }
 
-  res.send({ message: 'Personagem criado com sucesso!' });
+  const character = await new Character({
+    name,
+    species,
+    house,
+    actor, 
+  })
+
+  await character.save()
+
+  res.send({ message: "Personagem criado com sucesso!" });
 });
 
 //getBYID
-app.get('/character/:id', (req, res) => {
+app.get("/character/:id", (req, res) => {
   const id = +req.params.id;
   const character = characters.find((c) => c.id === id);
   if (!character) {
-    res.status(404).send({ message: 'Personagem não existe!' });
+    res.status(404).send({ message: "Personagem não existe!" });
     return;
   }
   res.send(character);
 });
 
 //PUT - UPDATE
-app.put('/character/:id', (req, res) => {
+app.put("/character/:id", (req, res) => {
   const id = +req.params.id;
   const character = characters.find((c) => c.id === id);
   if (!character) {
-    res.status(404).send({ message: 'Personagem não existe!' });
+    res.status(404).send({ message: "Personagem não existe!" });
     return;
   }
 
-  const { name, spacies, house, actor } = req.body;
+  const { name, species, house, actor } = req.body;
 
   character.name = name;
-  character.spacies = spacies;
+  character.species = species;
   character.house = house;
   character.actor = actor;
 
@@ -66,20 +97,20 @@ app.put('/character/:id', (req, res) => {
 });
 
 //DELETE - DELETE
-app.delete('/character/:id', (req, res) => {
+app.delete("/character/:id", (req, res) => {
   const id = +req.params.id;
   const character = characters.find((c) => c.id === id);
   if (!character) {
-    res.status(404).send({ message: 'Personagem não existe!' });
+    res.status(404).send({ message: "Personagem não existe!" });
     return;
   }
 
   const indexCharacter = characters.indexOf(character);
   delete characters[indexCharacter];
 
-  res.send({ message: 'Personagem apagado com sucesso!' });
+  res.send({ message: "Personagem apagado com sucesso!" });
 });
 
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+  console.log("Servidor rodando em http://localhost:3000");
 });
