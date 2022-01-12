70e784515074f83a5277f50a99eb91cd75cd3864
flaviogabrielsc <flaviogabriel112890@gmail.com>
2022-01-12 10:25:12 -0300

    separando controladores e banco de dados do arquivo principal

---
+++ database/database.js
@@ -0,0 +1,22 @@
+const database = () => {
+    const mongoose = require("mongoose");
+
+
+    try {
+        mongoose.connect(
+          process.env.DATABASE_URI,
+          {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          }
+        );
+        console.log("Banco de dados conectado!");
+      } catch (err) {
+        console.log(`Erro ao conectar ao bando de dados ${err}`);
+      }
+
+module.exports = database()
+
+
+
+}
