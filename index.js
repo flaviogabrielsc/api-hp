fc95f328cf2805477b8682a30e273583db9da656
flaviogabrielsc <flaviogabriel112890@gmail.com>
2022-01-12 11:38:52 -0300

    separando controladores e banco de dados do arquivo principal

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const controllers = require('./routes/controllers.js') 
-const database = require('./database/database.js') 
+const controllers = require("./routes/controllers.js");
+const database = require("./database/database.js");
 
-app.use(express.json()); 
-const port = process.env.PORT || 3000 
+app.use(express.json());
+const port = process.env.PORT || 3000;
 
-app.use('/', controllers)
-app.use('/', database)
-
+app.use("/", controllers);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
